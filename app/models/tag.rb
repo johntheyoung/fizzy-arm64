85f49fe3cb45d113abbccb12b08b2bfa35d33a60
Jeffrey Hardy <jeff@37signals.com>
2024-09-13 18:29:17 -0400

    Remove speculative association and normalization

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,4 @@
 class Tag < ApplicationRecord
-  # belongs_to :account
-
   has_many :taggings, dependent: :destroy
   has_many :bubbles, through: :taggings
-
-  normalizes :title, with: ->(value) { value.to_s }
 end
